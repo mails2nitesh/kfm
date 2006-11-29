f13c5f758d988f9650843a2032bd27f7798c5942
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2006-11-29 22:08:02 +0000

    add ability to download files based on their database id instead of their filename.
    this allows the file repository to be located anywhere on the computer - it is no longer needed for the repository to be normally accessible by the casual web browser.
    
    also, added a choice for what happens when a file is double-clicked - is it downloaded, or is it passed to fckeditor

---
+++ get.php
@@ -0,0 +1,37 @@
+<?php
+# see license.txt for licensing
+require_once('config.php');
+$id=$_GET['id'];
+if(!is_numeric($id)){
+	echo 'error: invalid id'; # TODO: new string
+	exit;
+}
+$q=$db->query('select physical_address,files.name as name from directories,files where directory=directories.id and files.id='.$id);
+$r=$q->fetchRow();
+if(!count($r)){
+	echo 'error: file id #'.$id.' not found in database'; # TODO: new string
+	exit;
+}
+$path=$r['physical_address'].'/'.$r['name'];
+$mime=isset($_GET['forcedownload'])?'force/download':mime_content_type($path);
+$name=$r['name'];
+if(strstr($_SERVER['HTTP_USER_AGENT'],'MSIE'))$name=preg_replace('/\./','%2e',$name,substr_count($name,'.')-1);
+
+set_time_limit(0);
+header("Cache-Control: ");
+header("Pragma: ");
+header('Content-Type: '.$mime);
+header('Content-Length: '.(string)(filesize($path)));
+header('Content-Disposition: attachment; filename="'.$name.'"');
+header('Content-Transfer-Encoding: binary');
+
+if($file=fopen($path,'rb')){
+	while((!feof($file))&&(connection_status()==0)){
+		print(fread($file,1024*8));
+		flush();
+	}
+	fclose($file);
+}
+return((connection_status()==0) and !connection_aborted());
+
+?>
