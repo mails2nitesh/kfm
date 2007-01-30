87d2c7391663ba00a5560340d06cab887c0f9876
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-01-30 09:20:05 +0000

    remove redundant mkdir line

--- trunk/initialise.php
+++ trunk/initialise.php
@@ -43,7 +43,6 @@ set_error_handler('kfm_error_log');
 { # variables
 	define('KFM_VERSION',rtrim(file_get_contents('version.txt')));
 	$rootdir=str_replace('//','/',$_SERVER['DOCUMENT_ROOT'].$kfm_userfiles.'/');
-	if(!file_exists($rootdir.$kfm_workdirectory))mkdir($rootdir.$kfm_workdirectory);
 	if(!isset($_SESSION['kfm']))$_SESSION['kfm']=array(
 		'currentdir'=>rtrim($rootdir,' /'),
 		'cwd_id'=>1,
