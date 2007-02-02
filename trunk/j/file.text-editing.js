36182efdd8a7c4915eb72dcdb5fee06990998820
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-02-02 08:05:38 +0000

    0000150: if you are editing a file, and click a directory in the left pane, you lose your changes

--- trunk/j/file.text-editing.js
+++ trunk/j/file.text-editing.js
@@ -16,16 +16,22 @@ function kfm_createEmptyFile(){
 	}while(not_ok);
 	x_kfm_createEmptyFile(filename,kfm_refreshFiles);
 }
+function kfm_disableLeftColumn(){
+	var left_column=$('kfm_left_column'),blocker=newEl('div','kfm_left_column_hider');
+	blocker.setCss('position:absolute;left:0;top:0;width:'+left_column.offsetWidth+'px;height:'+left_column.offsetHeight+'px;opacity:.7;background:#fff');
+	document.body.addEl(blocker);
+}
 function kfm_editTextFile(id){
 	x_kfm_getTextFile(id,kfm_showTextFile);
 }
 function kfm_showTextFile(res){
+	if(!$('kfm_left_column_hider'))kfm_disableLeftColumn();
 	var t=newEl('table','kfm_editFileTable').setCss('height:100%;width:100%');
 	var r2=t.addRow();
 	r2.addCell(0,1,res.name);
-	r2.addCell(1,1,newLink('javascript:x_kfm_viewTextFile('+res.id+',kfm_viewTextFile)','View',0,'button'));
-	r2.addCell(2,1,newLink('javascript:kfm_setMessage("saving file...");$("codepress-ondemand").value=CodePress.getCode();x_kfm_saveTextFile('+res.id+',$("codepress-ondemand").value,kfm_clearMessage);','Save',0,'button'));
-	r2.addCell(3,1,newLink('javascript:if($("codepress-ondemand").value==CodePress.getCode() || confirm( kfm_lang.CloseWithoutSavingQuestion))x_kfm_loadFiles(kfm_cwd_id,kfm_refreshFiles);',kfm_lang.Close,0,'button'));
+	r2.addCell(1,1,newLink('javascript:delEl("kfm_left_column_hider");x_kfm_viewTextFile('+res.id+',kfm_viewTextFile)','View',0,'button'));
+	r2.addCell(2,1,newLink('javascript:delEl("kfm_left_column_hider");kfm_setMessage("saving file...");$("codepress-ondemand").value=CodePress.getCode();x_kfm_saveTextFile('+res.id+',$("codepress-ondemand").value,kfm_clearMessage);','Save',0,'button'));
+	r2.addCell(3,1,newLink('javascript:if($("codepress-ondemand").value==CodePress.getCode() || confirm( kfm_lang.CloseWithoutSavingQuestion)){delEl("kfm_left_column_hider");x_kfm_loadFiles(kfm_cwd_id,kfm_refreshFiles);}',kfm_lang.Close,0,'button'));
 	r3=t.addRow().setCss('height:100%').addCell(0,4);
 	r3.innerHTML = '<iframe id="codepress" src="codepress/codepress.php" width="100%" height="100%"></iframe><textarea id="codepress-ondemand" name="codepress-ondemand" style="display:none" lang="'+res.lang+'"></textarea>';
 	$('kfm_right_column').empty().addEl(t);
