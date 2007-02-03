5351d30f446197204f382257df0decdb2deadfe8
bterkuile <bterkuile@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-02-03 15:04:43 +0000

    Update CodePress to version 0.9 with some style issues to be solved

--- codepress/codepress.js
+++ codepress/codepress.js
@@ -8,175 +8,166 @@
  * 
  * Read the full licence: http://www.opensource.org/licenses/lgpl-license.php
  */
-
+ 
+$ = function() { return document.getElementById(arguments[0]); }
+var cpPath = $('cp-script').src.replace('codepress.js','');
+document.write('<link type="text/css" href="'+cpPath+'themes/default/codepress-editor.css" rel="stylesheet" />');
+ 
 CodePress = {
-	range : null,
-	language : null,
-	scrolling : false,
-		
-	// set initial vars and start sh
 	initialize : function() {
-		if(typeof(editor)=='undefined'&&!arguments[0]) return;
-		this.detect();
-		chars = '|13|32|191|57|48|187|188|'; // charcodes that trigger syntax highlighting
-		cc = '\u2009'; // control char
-		if(browser.ff) {
-			editor = document.getElementById('ffedt');
-			document.designMode = 'on';
-			document.addEventListener('keydown', this.keyHandler, true);
-			window.addEventListener('scroll', function() { if(!CodePress.scrolling) CodePress.syntaxHighlight('scroll') }, false);
+		cpWindow = $('cp-window');
+		cpEditor = $('cp-editor');
+		cpBody = cpEditor.contentWindow;
+		cpBody.CodePress.syntaxHighlight('init');
+		if(onLoadEdit) { onLoadEdit=false; this.edit(this.fileName,pgCode); }
+	},
+	
+	detect : function() {
+		cpEngine = 'older';
+		var ua = navigator.userAgent;
+		if(ua.match('MSIE')) cpEngine = 'msie';
+		else if(ua.match('KHTML')) cpEngine = 'khtml'; 
+		else if(ua.match('Opera')) cpEngine = 'opera'; 
+		else if(ua.match('Gecko')) cpEngine = 'gecko';
+	},
+	
+	setFileName : function() {
+		$('cp-filename').innerHTML = this.fileName = (arguments[0]) ? arguments[0] : Content.menu.untitledFile;
+	},
+
+	getLanguage : function() {
+		var extension = this.fileName.replace(/.*\.([^\.]+)$/,'$1');
+		for(lang in Content.languages) {
+			extensions = ','+Content.languages[lang].extensions+',';
+			if(extensions.match(','+extension+',')) return lang;
 		}
-		else if(browser.ie) {
-			editor = document.getElementById('ieedt');
-			editor.contentEditable = 'true';
-			document.attachEvent('onkeydown', this.keyHandler);
-			window.attachEvent('onscroll', function() { if(!CodePress.scrolling) CodePress.syntaxHighlight('scroll') });
+		return 'generic';
+	},
+
+	loadScript : function(target, src, callback) {
+		var node = target.createElement("script");
+		if (node.addEventListener) node.addEventListener("load", callback, false);
+		else node.onreadystatechange = function() { if (this.readyState == "loaded") { callback.call(this);} }
+		node.src = src;
+		target.getElementsByTagName("head").item(0).appendChild(node);
+		node = null;
+	},
+	
+	setLanguage : function(reload) {
+		this.language = (typeof(Content.languages[arguments[0]])!='undefined') ? arguments[0] : this.getLanguage();
+		$('cp-language-name').innerHTML = Content.languages[this.language].name;
+		$('language-'+this.language).checked = true;
+		this.hideAllMenu();
+		if(reload) {
+			if(cpBody.document.designMode=='on') cpBody.document.designMode = 'off';
+			this.loadScript(cpBody.document, '../languages/'+this.language+'.js', function () { cpBody.CodePress.syntaxHighlight('init'); })
+			cpBody.document.getElementById('cp-lang-style').href = '../languages/'+this.language+'.css';
 		}
-		else {
-			// TODO: textarea without syntax highlighting for non supported browsers
-			alert('your browser is not supported at the moment');
+	},
+	
+	hideAllMenu : function() {
+		$('cp-options-menu').className = $('cp-languages-menu').className = 'hide';
+		$('cp-arrow-languages').src = $('cp-arrow-options').src = cpPath+'themes/default/menu-arrow-up.gif' ;		
+	},
+	
+	toogleMenu : function(item) {
+		$('cp-'+item+'-menu').className = ($('cp-'+item+'-menu').className=='show') ? 'hide' : 'show' ;
+		$('cp-arrow-'+item).src = ($('cp-'+item+'-menu').className=='show') ? cpPath+'themes/default/menu-arrow-down.gif' : cpPath+'themes/default/menu-arrow-up.gif' ;
+	},
+	
+	toggleComplete : function() {
+		this.complete = $('cp-complete').checked ? true : false ;
+		this.hideAllMenu();
+	},
+
+	toggleLineNumbers : function() {
+	    cpBody.document.getElementsByTagName('body')[0].className = ($('cp-linenumbers').checked) ? 'show-line-numbers' : 'hide-line-numbers';
+		this.hideAllMenu();
+	},
+	
+	resizeFullScreen : function() {
+		if($('cp-fullscreen').checked) {
+			cH = self.innerHeight ? self.innerHeight : document.documentElement.clientHeight;
+			cW = self.innerWidth ? self.innerWidth : document.documentElement.clientWidth;
+			cpEditor.style.height = cH-22 + 'px';
+			cpWindow.style.height = cH-2 + 'px';
+			cpWindow.style.width = cW-2 + 'px';
+			if(cpWindow.offsetParent.offsetTop!=0||cpWindow.offsetParent.offsetLeft!=0) {
+				cpWindow.style.top = - cpWindow.offsetParent.offsetTop-3 +'px';
+				cpWindow.style.left = - cpWindow.offsetParent.offsetLeft-3 +'px';
+			}
+
+		}
+	},
+	
+	toggleFullScreen : function() {
+	    if($('cp-fullscreen').checked) {
+			cpWindow.className = 'fullscreen-on';
+			document.getElementsByTagName('html')[0].style.overflow = 'hidden';
+			self.scrollTo(0,0); 
+			this.resizeFullScreen();
+	    }
+	    else {
+			cpWindow.className = 'fullscreen-off';
+			document.getElementsByTagName('html')[0].style.overflow = 'auto';
+			cpWindow.style.width = '100%';
+			cpWindow.style.height = cpWindowHeight+'px';
+			cpEditor.style.height = cpEditorHeight+'px';
+			if(cpWindow.offsetParent.offsetTop!=0||cpWindow.offsetParent.offsetLeft!=0) 
+			cpWindow.style.top = cpWindow.style.left = 'auto'
+	    }
+		this.hideAllMenu();
+	},
+
+	edit : function() {
+		this.setFileName(arguments[0]);
+		if(!arguments[1]||arguments[1]=='') { // file name of the source code (to open from server)
+			this.setLanguage();
+			cpEditor.src = cpPath+'modules/codepress.php?action=edit&file='+this.fileName+'&language='+this.language+'&engine='+cpEngine
 			return;
 		}
-		this.syntaxHighlight('init');
-		setTimeout(function() { window.scroll(0,0) },50); // scroll IE to top
+		this.setLanguage('reload');
+		if($(arguments[1])) CodePress.setCode($(arguments[1]).firstChild.nodeValue); // id name of the source code
+		else if(arguments[1].match(/\w/)) CodePress.setCode(arguments[1]);  // text of the source code
+		else if(typeof(arguments[1])=='Object') CodePress.setCode(arguments[1].firstChild.nodeValue); // object of the source code
 	},
 
-	// detect browser, for now IE and FF
-	detect : function() {
-		browser = { ie:false, ff:false };
-		if(navigator.appName.indexOf("Microsoft") != -1) browser.ie = true;
-		else if(navigator.appName == "Netscape") browser.ff = true;
-	},
-
-	// treat key bindings
-	keyHandler : function(evt) {
-		evt = (evt) ? evt : (window.event) ? event : null;
-	  	if(evt) {
-	    	charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode : ((evt.which) ? evt.which : 0));
-
-			if((charCode==34||charCode==33)&&browser.ie) { // handle page up/down for IE
-				parent.codepress.scrollBy(0, (charCode==34) ? 200 : -200); 
-				evt.returnValue = false;
-			}
-		    if((chars.indexOf('|'+charCode+'|')!=-1) && (!evt.ctrlKey && !evt.altKey)) { // syntax highlighting
-			 	CodePress.syntaxHighlight('generic');
-			}
-			else if(charCode==46||charCode==8) { // save to history when delete or backspace pressed
-			 	CodePress.actions.history[CodePress.actions.next()] = editor.innerHTML;
-			}
-			else if((charCode==90||charCode==89) && evt.ctrlKey) { // undo and redo
-				(charCode==89||evt.shiftKey) ? CodePress.actions.redo() : CodePress.actions.undo() ;
-				evt.returnValue = false;
-				if(browser.ff)evt.preventDefault();
-			}
-			else if(charCode==86 && evt.ctrlKey)  { // paste
-				// TODO: pasted text should be parsed and highlighted
-			}
-
-		}
-	},
-
-	// put cursor back to its original position after every parsing
-	findString : function() {
-		if(browser.ff) {
-			if(self.find(cc))
-				window.getSelection().getRangeAt(0).deleteContents();
-		}
-		else if(browser.ie) {
-		    range = self.document.body.createTextRange();
-			if(range.findText(cc)){
-				range.select();
-				range.text = '';
-			}
-		}
-	},
-	
-	// split big files, highlighting parts of it
-	split : function(code,flag) {
-		if(flag=='scroll') {
-			this.scrolling = true;
-			return code;
-		}
-		else {
-			this.scrolling = false;
-			mid = code.indexOf(cc);
-			if(mid-2000<0) {ini=0;end=4000;}
-			else if(mid+2000>code.length) {ini=code.length-4000;end=code.length;}
-			else {ini=mid-2000;end=mid+2000;}
-			code = code.substring(ini,end);
-			if(browser.ff) return code;
-			else return code.substring(code.indexOf('<P>'),code.lastIndexOf('</P>')+4);
-		}
-	},
-	
-	// syntax highlighting parser
-	syntaxHighlight : function(flag) {
-		if(browser.ff) {
-			if(flag!='init') window.getSelection().getRangeAt(0).insertNode(document.createTextNode(cc));
-			o = editor.innerHTML;
-			o = o.replace(/<br>/g,'\n');
-			o = o.replace(/<.*?>/g,'');
-			x = z = this.split(o,flag);
-			x = x.replace(/\n/g,'<br>');
-		}
-		else if(browser.ie) {
-			if(flag!='init') document.selection.createRange().text = cc;
-			o = editor.innerHTML;
-			o = o.replace(/<P>/g,'\n');
-			o = o.replace(/<\/P>/g,'\r');
-			o = o.replace(/<.*?>/g,'');
-			o = '<PRE><P>'+o+'</P></PRE>';
-			o = o.replace(/\n/g,'<P>');
-			o = o.replace(/\r/g,'<\/P>');
-			o = o.replace(/<P>(<P>)+/,'<P>');
-			o = o.replace(/<\/P>(<\/P>)+/,'</P>');
-			o = o.replace(/<P><\/P>/g,'<P>&nbsp;<\/P>');
-			x = z = this.split(o,flag);
-		}
-
-		for(i=0;i<syntax.length;i+=2) 
-			x = x.replace(syntax[i],syntax[i+1]);
-
-		editor.innerHTML = this.actions.history[this.actions.next()] = (flag=='scroll') ? x : o.replace(z,x);
-
-		if(flag!='init') this.findString();
-	},
-
-	// undo and redo methods
-	actions : {
-		pos : -1, // actual history position
-		history : [], // history vector
+	setContent : function() {
+		var allLanguages = '';
+		for(lang in Content.languages) 
+			allLanguages += '<input type=radio name=lang id="language-'+lang+'" onclick="CodePress.setLanguage(\''+lang+'\')"><label for="language-'+lang+'">'+Content.languages[lang].name+'</label><br />';
 		
-		undo : function() {
-			if(editor.innerHTML.indexOf(cc)==-1){
-				if(browser.ff) window.getSelection().getRangeAt(0).insertNode(document.createTextNode(cc));
-				else document.selection.createRange().text = cc;
-			 	this.history[this.pos] = editor.innerHTML;
-			}
-			this.pos--;
-			if(typeof(this.history[this.pos])=='undefined') this.pos++;
-			editor.innerHTML = this.history[this.pos];
-			CodePress.findString();
-		},
+		pgCode = ($('codepress').firstChild) ? $('codepress').firstChild.nodeValue : '';
+		this.fileName = $('codepress').title;
+		this.language = this.getLanguage();
+		this.complete = true;
+		onLoadEdit = (pgCode.match(/\w/)) ? true : false ;
+
+		cpWindowHeight = $('codepress').clientHeight;
+		cpEditorHeight = ($('codepress').className.match('hideMenu')) ? cpWindowHeight : cpWindowHeight-20 ;
+		if(cpWindowHeight==0) { setTimeout(function(){CodePress.setContent();},10); return; } // if css is not loaded yet, try again
 		
-		redo : function() {
-			this.pos++;
-			if(typeof(this.history[this.pos])=='undefined') this.pos--;
-			editor.innerHTML = this.history[this.pos];
-			CodePress.findString();
-		},
+		$('codepress').innerHTML = '<div id="cp-window">'+
+			'<iframe id="cp-editor" src="'+cpPath+'modules/codepress.php?engine='+cpEngine+'&file='+ (onLoadEdit ? 'null' : this.fileName) +'&language='+this.language+'" style="height:'+ cpEditorHeight +'px"></iframe>'+
+			'<div id="cp-menu">'+
+				'<em id="cp-filename"></em><span id="cp-options" onclick="CodePress.toogleMenu(\'options\')"><img src="'+cpPath+'themes/default/menu-icon-options.gif" align="top" /> '+Content.menu.options+' <img src="'+cpPath+'themes/default/menu-arrow-up.gif" align="top" id="cp-arrow-options" /></span><span id="cp-language" onclick="CodePress.toogleMenu(\'languages\')"><img src="'+cpPath+'themes/default/menu-icon-languages.gif" align="top" /> <span id="cp-language-name">'+Content.languages.generic.name+'</span> <img src="'+cpPath+'themes/default/menu-arrow-up.gif" align=top id="cp-arrow-languages" /></span>'+
+			'</div>'+
+			'<div id="cp-options-menu" class="hide">'+
+    			'<input type="checkbox" id="cp-fullscreen" onclick="CodePress.toggleFullScreen()"><label for="cp-fullscreen">'+Content.menu.fullScreen+'</label><br><input type=checkbox id="cp-linenumbers" onclick="CodePress.toggleLineNumbers()" checked="checked"><label for="cp-linenumbers">'+Content.menu.lineNumbers+'</label><br><input type=checkbox id="cp-complete" onclick="CodePress.toggleComplete()" checked="checked"><label for="cp-complete">'+Content.menu.autoComplete+'</label>'+
+			'</div>'+
+			'<div id="cp-languages-menu" class="hide">'+allLanguages+'</div>'+
+		'</div>';
 		
-		next : function() { // get next vector position and clean old ones
-			if(this.pos>20) this.history[this.pos-21] = undefined;
-			return ++this.pos;
-		}
-	},	
-	
+		this.setLanguage(); 
+		this.setFileName(this.fileName); 
+	},
+
 	// transform syntax highlighted code to original code
 	getCode : function() {
-		code = editor.innerHTML;
-		code = code.replace(/<br>/gi,'\n');
+		var code = cpBody.editor.innerHTML;
+		code = code.replace(/<br>/g,'\n');
 		code = code.replace(/<\/p>/gi,'\r');
+		code = code.replace(/<p>/i,''); // IE first line fix
 		code = code.replace(/<p>/gi,'\n');
 		code = code.replace(/&nbsp;/gi,'');
 		code = code.replace(/\u2009/g,'');
@@ -189,47 +180,16 @@ CodePress = {
 
 	// put some code inside editor
 	setCode : function() {
-		if(typeof(arguments[1])=='undefined') {
-			language = top.document.getElementById(arguments[0]).lang.toLowerCase();
-			code = top.document.getElementById(arguments[0]).value;
-		} 
-		else {
-			language = arguments[0];
-			code = arguments[1];
-		}
-		document.designMode = 'off';
-	   	head = document.getElementsByTagName('head')[0];
-	   	script = document.createElement('script');
-	   	script.type = 'text/javascript';
-	   	script.src = 'languages/codepress-'+language+'.js';
-		head.appendChild(script)
-		document.getElementById('cp-lang-style').href = 'languages/codepress-'+language+'.css';
+		var code = arguments[0];
 		code = code.replace(/\u2009/gi,'');
 		code = code.replace(/&/gi,'&amp;');		
        	code = code.replace(/</g,'&lt;');
         code = code.replace(/>/g,'&gt;');
-		editor.innerHTML = "<pre>"+code+"</pre>";
-		this.language = language;
+		cpBody.document.getElementById("code").innerHTML = "<pre>"+code+"</pre>";
 	}
 }
 
-onload = function() {
-	cpWindow = top.document.getElementById('codepress');
-	if(cpWindow!=null) {
-		cpWindow.style.border = '1px solid gray';
-		cpWindow.style.frameBorder = '0';
-	}
-	
-	top.CodePress = CodePress;
-	CodePress.initialize('new');
-	
-	cpOnload = top.document.getElementById('codepress-onload');
-	cpOndemand = top.document.getElementById('codepress-ondemand');
-	
-	if(cpOnload!=null) {
-		cpOnload.style.display = 'none';
-		cpOnload.id = 'codepress-loaded';
-		CodePress.setCode('codepress-loaded');
-	}
-	if(cpOndemand!=null) cpOndemand.style.display = 'none';
-}
+CodePress.detect();
+Content={};
+CodePress.loadScript(document, cpPath+'content/'+$('cp-script').lang+'.js', function() {}); 
+onresize = function() { CodePress.resizeFullScreen(); };
