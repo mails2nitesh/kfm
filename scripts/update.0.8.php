28c953acfce79dfcd16f02721c2b4c8e777953ab
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-02-09 06:21:18 +0000

    as usual, forget the important file...

---
+++ scripts/update.0.8.php
@@ -0,0 +1,22 @@
+<?php
+if($kfm_parameters['version']=='7.0'||$kfm_parameters['version']<'0.7.2')require 'scripts/update.0.7.2.php';
+switch($kfm_db_type){
+	case 'mysql':{
+		require 'scripts/db.mysql.update.0.7.2.php';
+		break;
+	}
+	case 'pgsql':{
+		require 'scripts/db.pgsql.update.0.7.2.php';
+		break;
+	}
+	case 'sqlite':{
+		require 'scripts/db.sqlite.update.0.7.2.php';
+		break;
+	}
+	default:{
+		echo 'error: unknown database specified in scripts/update.0.7.2.php'; # TODO: new string
+		exit;
+	}
+}
+$db->query("update ".$kfm_db_prefix."parameters set value='0.8' where name='version'");
+?>
