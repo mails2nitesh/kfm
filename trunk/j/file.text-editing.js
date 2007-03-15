e4d44caaad2b7c0c2bf4af87fccf385f0d3f8d88
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-03-15 14:59:50 +0000

    remove unused function, and shorten code used for element creation

--- trunk/j/file.text-editing.js
+++ trunk/j/file.text-editing.js
@@ -33,25 +33,18 @@ function kfm_showTextFile(res){
 	var r2=t.addRow();
 	r2.addCell(0,1,res.name);
 	r2.addCell(1,1,newLink('javascript:delEl("kfm_left_column_hider");x_kfm_viewTextFile('+res.id+',kfm_viewTextFile)','View',0,'button'));
-	r2.addCell(2,1,newLink('javascript:delEl("kfm_left_column_hider");kfm_setMessage("saving file...");$("edit-start").value=CodePress.getCode();x_kfm_saveTextFile('+res.id+',$("edit-start").value,kfm_clearMessage);','Save',0,'button'));
+	r2.addCell(2,1,newLink('javascript:kfm_setMessage("saving file...");$("edit-start").value=CodePress.getCode();x_kfm_saveTextFile('+res.id+',$("edit-start").value,kfm_clearMessage);','Save',0,'button'));
 	r2.addCell(3,1,newLink('javascript:if($("edit-start").value==CodePress.getCode() || confirm( kfm_lang.CloseWithoutSavingQuestion)){delEl("kfm_left_column_hider");x_kfm_loadFiles(kfm_cwd_id,kfm_refreshFiles);}',kfm_lang.Close,0,'button'));
 	r3=t.addRow().setCss('height:100%').addCell(0,4);
-	codeEl = document.createElement('code');
-	codeEl.innerHTML = res.content;
-	codeEl.setAttribute('id','codepress');
-	codeEl.setAttribute('title',res.name);
-	codeEl.setAttribute('class','cp hideLanguage');
-	codeEl.setAttribute('style','width:100%;height:'+(r3.offsetHeight-2)+'px;');
-	
-	changeCheckEl = document.createElement('textarea');
-	changeCheckEl.value = res.content;
-	changeCheckEl.setAttribute('id','edit-start');
-	changeCheckEl.setAttribute('style','display:none;');
+	r3.id='kfm_codepressTableCell';
+	var codeEl=newEl('code','codepress','cp hideLanguage');
+	codeEl.innerHTML=res.content;
+	codeEl.title=res.name;
+	codeEl.setCss('width:100%;height:'+(r3.offsetHeight-2)+'px');
+	changeCheckEl=newInput('edit-start','textarea',res.content);
+	changeCheckEl.setCss('display:none');
 	r3.appendChild(codeEl);
 	r3.appendChild(changeCheckEl);
-	CodePress.setContent();
-}
-function CodePressLoadCode(){
 	CodePress.setContent();
 }
 function kfm_viewTextFile(res){
