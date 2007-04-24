f96aa54904110808363c5260e39c3584e639de2e
bterkuile <bterkuile@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-04-24 15:14:18 +0000

    The start of the kfmDirectory object, not complete, but should work already

--- framework.php
+++ framework.php
@@ -2,4 +2,5 @@
 require $kfm_base_path.'includes/object.class.php';
 require $kfm_base_path.'includes/file.class.php';
 require $kfm_base_path.'includes/image.class.php';
+require $kfm_base_path.'includes/directory.class.php';
 ?>
