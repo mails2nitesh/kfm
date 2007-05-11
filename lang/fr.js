3253241f982d908ddd50977ee1dda8b1d4570b2c
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-05-11 20:04:38 +0000

    Dutch language update, thanks to Roy Lubbers French language update, thanks to Hubert Garrido

--- lang/fr.js
+++ lang/fr.js
@@ -259,14 +259,14 @@ WhichTagsDoYouWantToRemove:
 
 ,
 // New in 0.9
-AllFiles: "all files",
-AndNMore: "...and %1 more...",
-Browse: "Browse...",
-ExtractAfterUpload: "extract after upload",
-NotAnImageOrImageDimensionsNotReported: "error: not an image, or image dimensions not reported",
-PermissionDeniedCannotDeleteFile: "permission denied: cannot delete file",
-RenameTheDirectoryToWhat: "Rename the directory '%1' to what?",
-RenamedDirectoryAs: "Renamed '%1' as '%2'",
-TheFilenameShouldEndWithN: "The filename should end with %1",
-WhatFilenameDoYouWantToUse: "What filename do you want to use?"
+AllFiles: "tous les fichiers",
+AndNMore: "...et %1 autres...",
+Browse: "Parcourir...",
+ExtractAfterUpload: "décompresser après envoi",
+NotAnImageOrImageDimensionsNotReported: "erreur: pas une image, ou impossible de déterminer ses dimensions",
+PermissionDeniedCannotDeleteFile: "permission refusée: impossible de supprimer le fichier",
+RenameTheDirectoryToWhat: "Renommer le dossier '%1' en?",
+RenamedDirectoryAs: "dossier '%1' renommé en '%2'",
+TheFilenameShouldEndWithN: "Le nom du fichier devrait finir par %1",
+WhatFilenameDoYouWantToUse: "Quel nom de fichier souhaitez-vous utiliser?"
 }
