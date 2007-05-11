3ef774a9ac333f847b26d473113b9843aed5a549
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-05-11 20:04:38 +0000

    Dutch language update, thanks to Roy Lubbers French language update, thanks to Hubert Garrido

--- trunk/lang/nl.js
+++ trunk/lang/nl.js
@@ -254,14 +254,14 @@ WhichTagsDoYouWantToRemove:
 
 ,
 // New in 0.9
-AllFiles: "all files",
-AndNMore: "...and %1 more...",
-Browse: "Browse...",
-ExtractAfterUpload: "extract after upload",
-NotAnImageOrImageDimensionsNotReported: "error: not an image, or image dimensions not reported",
-PermissionDeniedCannotDeleteFile: "permission denied: cannot delete file",
-RenameTheDirectoryToWhat: "Rename the directory '%1' to what?",
-RenamedDirectoryAs: "Renamed '%1' as '%2'",
-TheFilenameShouldEndWithN: "The filename should end with %1",
-WhatFilenameDoYouWantToUse: "What filename do you want to use?"
+AllFiles: "alle bestanden",
+AndNMore: "...en %1 meer...",
+Browse: "Bladeren...",
+ExtractAfterUpload: "uitpakken na upload",
+NotAnImageOrImageDimensionsNotReported: "fout: geen afbeelding of geen bestandsdimensies aanwezig",
+PermissionDeniedCannotDeleteFile: "toegang geweigerd: kan bestand niet verwijderen",
+RenameTheDirectoryToWhat: "Hernoem map '%1' naar?",
+RenamedDirectoryAs: "'%1' hernoemd als '%2'",
+TheFilenameShouldEndWithN: "Bestandsnaam moet eindigen met %1",
+WhatFilenameDoYouWantToUse: "Welke bestandsnaam wilt u gebruiken?"
 }
