584f7569c1aff8f1baea30db33886ca292eb0d39
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-06-25 21:07:57 +0000

    0000175: mysql tables should be UTF8 instead of Latin1
    and hopefully,
    0000240: some files with non-ascii characters are impossible to work with

--- trunk/scripts/db.mysql.create.php
+++ trunk/scripts/db.mysql.create.php
@@ -1,44 +1,46 @@
 <?php
-	$kfmdb->query("create table ".$kfm_db_prefix."parameters(name text, value text)");
-	$kfmdb->query("create table ".$kfm_db_prefix."directories(
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."directories(
 		id INTEGER PRIMARY KEY auto_increment,
 		name text,
-		physical_address text,
 		parent integer not null
-	)");
-	$kfmdb->query("create table ".$kfm_db_prefix."files(
+	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."files(
 		id INTEGER PRIMARY KEY auto_increment,
 		name text,
 		directory integer not null,
 		foreign key (directory) references ".$kfm_db_prefix."directories(id)
-	)");
-	$kfmdb->query("create table ".$kfm_db_prefix."files_images(
+	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."files_images(
 		id INTEGER PRIMARY KEY auto_increment,
 		caption text,
 		file_id integer not null,
 		width integer default 0,
 		height integer default 0,
 		foreign key (file_id) references ".$kfm_db_prefix."files (id)
-	)");
-	$kfmdb->query("create table ".$kfm_db_prefix."files_images_thumbs(
+	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."files_images_thumbs(
 		id INTEGER PRIMARY KEY auto_increment,
 		image_id integer not null,
 		width integer default 0,
 		height integer default 0,
 		foreign key (image_id) references ".$kfm_db_prefix."files_images (id)
-	)");
-	$kfmdb->query("create table ".$kfm_db_prefix."tags(
-		id INTEGER PRIMARY KEY auto_increment,
-		name text
-	)");
-	$kfmdb->query("create table ".$kfm_db_prefix."tagged_files(
+	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."parameters(
+		name text,
+		value text
+	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."tagged_files(
 		file_id	INTEGER,
 		tag_id	INTEGER,
 		foreign key (file_id) references ".$kfm_db_prefix."files (id),
 		foreign key (tag_id) references ".$kfm_db_prefix."tags (id)
-	)");
+	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."tags(
+		id INTEGER PRIMARY KEY auto_increment,
+		name text
+	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
 
-	$kfmdb->query("insert into ".$kfm_db_prefix."parameters values('version','0.9')");
+	$kfmdb->query("insert into ".$kfm_db_prefix."parameters values('version','0.9.1')");
 	$res=$kfmdb->query("insert into ".$kfm_db_prefix."directories values(1,'','".rtrim(addslashes($rootdir),' /')."',0)");
 	if(!PEAR::isError($res))$_SESSION['db_defined']=1;
 ?>
