5551d47fb9680d1832d44af30d82c0cbb3f45c40
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-06-25 21:07:57 +0000

    0000175: mysql tables should be UTF8 instead of Latin1
    and hopefully,
    0000240: some files with non-ascii characters are impossible to work with

--- j/file.selections.js
+++ j/file.selections.js
@@ -48,7 +48,7 @@ function kfm_file_dragFinish(e){
 	if(dir_over=='.'||dir_over==kfm_cwd_id)return;
 	{ // build context menu for "copy/move"
 		var links=[];
-		links.push(['x_kfm_copyFiles(['+selectedFiles.join(',')+'],'+dir_over+',kfm_alert);kfm_selectNone()','copy files']);
+		links.push(['x_kfm_copyFiles(['+selectedFiles.join(',')+'],'+dir_over+',kfm.alert);kfm_selectNone()','copy files']);
 		links.push(['x_kfm_moveFiles(['+selectedFiles.join(',')+'],'+dir_over+',function(){kfm_removeFilesFromView(['+selectedFiles.join(',')+'])});kfm_selectNone()','move files',0,!kfm_vars.permissions.file.mv]);
 		kfm_createContextMenu(getMouseAt(getEvent(e)),links);
 	}
