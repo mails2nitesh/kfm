345add62b7ad47bf9e0335b3ac9d8b7d9e5a64ab
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-07-01 12:07:52 +0000

    forgot the create scripts, and the updates assumed a prefix of 'kfm_'

--- trunk/scripts/db.mysql.create.php
+++ trunk/scripts/db.mysql.create.php
@@ -29,6 +29,19 @@
 		name text,
 		value text
 	)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."session (
+		`id` int(11) NOT NULL auto_increment,
+		`cookie` varchar(32) default NULL,
+		`last_accessed` datetime default NULL,
+		PRIMARY KEY  (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."session_vars (
+		`session_id` int(11) default NULL,
+		`varname` text,
+		`varvalue` text,
+		KEY `session_id` (`session_id`),
+		CONSTRAINT `".$kfm_db_prefix."session_vars_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `".$kfm_db_prefix."session` (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8");
 	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."tagged_files(
 		file_id	INTEGER,
 		tag_id	INTEGER,
--- trunk/scripts/db.pgsql.create.php
+++ trunk/scripts/db.pgsql.create.php
@@ -31,6 +31,19 @@
 		primary key (id),
 		foreign key (image_id) references ".$kfm_db_prefix."files_images(id)
 	)");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."session (
+		`id` int(11) NOT NULL auto_increment,
+		`cookie` varchar(32) default NULL,
+		`last_accessed` datetime default NULL,
+		PRIMARY KEY  (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."session_vars (
+		`session_id` int(11) default NULL,
+		`varname` text,
+		`varvalue` text,
+		KEY `session_id` (`session_id`),
+		CONSTRAINT `".$kfm_db_prefix."session_vars_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `".$kfm_db_prefix."session` (`id`)
+	)");
 	$kfmdb->query("create table ".$kfm_db_prefix."tags(
 		id serial,
 		name text,
