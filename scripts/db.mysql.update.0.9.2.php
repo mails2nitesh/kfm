d57a5c13ef66b98b61ff979448332613db76eec6
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-07-02 10:58:12 +0000

    how did that happen???

--- scripts/db.mysql.update.0.9.2.php
+++ scripts/db.mysql.update.0.9.2.php
@@ -1,2 +1,15 @@
 <?php
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."session (
+		`id` int(11) NOT NULL auto_increment,
+		`cookie` varchar(32) default NULL,
+		`last_accessed` datetime default NULL,
+		PRIMARY KEY  (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+	$kfmdb->query("CREATE TABLE ".$kfm_db_prefix."session_vars (
+		`session_id` int(11) default NULL,
+		`varname` text,
+		`varvalue` text,
+		KEY `session_id` (`session_id`),
+		CONSTRAINT `".$kfm_db_prefix."session_vars_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `".$kfm_db_prefix."session` (`id`)
+	)");
 ?>
