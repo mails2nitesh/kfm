88b17081d595d0bd5bd10f9bf2424c8985a0a6fb
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-07-09 12:21:17 +0000

    fix PostGres database

--- trunk/includes/session.class.php
+++ trunk/includes/session.class.php
@@ -20,7 +20,7 @@ class kfmSession extends Object{
 		}
 		if($create){
 			$this->db->query("INSERT INTO ".$this->db_prefix."session (last_accessed) VALUES (now())");
-			$this->id=$this->db->lastInsertId($kfm_db_prefix.'session','id');
+			$this->id=$this->db->lastInsertId($this->db_prefix.'session','id');
 			$key=md5($this->id);
 			$this->db->query("UPDATE ".$this->db_prefix."session SET cookie='".$key."' WHERE id=".$this->id);
 			$this->isNew=true;
