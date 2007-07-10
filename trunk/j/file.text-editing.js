0efdef2b526a23be65683fb9ace400341dbf4152
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-07-10 08:21:27 +0000

    fix bug where editing a file immediately upon loading KFM (before clicking any directories) and then clicking Close would cause a few JS errors (kfm_cwd_id was not set correctly)

--- trunk/j/file.text-editing.js
+++ trunk/j/file.text-editing.js
@@ -47,7 +47,7 @@ function kfm_showTextFile(res){
 	kfm.addCell(r2,0,1,res.name);
 	kfm.addCell(r2,1,1,newLink('javascript:$("kfm_left_column_hider").remove();x_kfm_viewTextFile('+res.id+',kfm_viewTextFile)','View',0,'button'));
 	kfm.addCell(r2,2,1,newLink('javascript:new Notice("saving file...");$("edit-start").value=codepress.getCode();x_kfm_saveTextFile('+res.id+',$("edit-start").value,kfm_showMessage);','Save',0,'button'));
-	kfm.addCell(r2,3,1,newLink('javascript:if($("edit-start").value==codepress.getCode() || kfm.confirm( kfm.lang.CloseWithoutSavingQuestion)){$("kfm_left_column_hider").remove();x_kfm_loadFiles(kfm_cwd_id,kfm_refreshFiles);}',kfm.lang.Close,0,'button'));
+	kfm.addCell(r2,3,1,newLink('javascript:if($("edit-start").value==codepress.getCode() || kfm.confirm( kfm.lang.CloseWithoutSavingQuestion)){$("kfm_left_column_hider").remove();kfm_changeDirectory("kfm_directory_icon_"+kfm_cwd_id);}',kfm.lang.Close,0,'button'));
 	r3=kfm.addCell(kfm.addRow(t).setStyles('height:100%'),0,4);
 	r3.id='kfm_codepressTableCell';
 	var codeEl=new Element('textarea',{
