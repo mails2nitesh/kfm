1d57f749e789a6183002f5e4b11ca0a2291a7038
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-07-29 16:29:06 +0000

    as usual, I've forgotten to add the relevant files...

---
+++ trunk/lang/de.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * See ../license.txt for licensing
+ *
+ * For further information visit:
+ *  http://kfm.verens.com/
+ *
+ * File Name: en.php
+ *  English language file.
+ *
+ * File Authors:
+ *  kae@verens.com
+ */
+
+$kfm_langStrings=array(
+	"alreadyExists"                => "\"%1\" already exists",
+	"errorInvalidID"               => "error: invalid ID",
+	"errorFileIDNotFound"          => "error: file id #%1 not found in database",
+	"bannedFilenameExtension"      => "banned extension in file name",
+	"cannotEditRestrictedExtension"=> "cannot edit \"%1\"\nrestricted by file extension",
+	"cannotMoveIntoSelf"           => "cannot move a directory into its own sub-directory",
+	"cannotRenameFromTo"           => "cannot rename \"%1\" to \"%2\"",
+	"CMSRefusesFileDelete"         => "CMS does not allow \"%1\" to be deleted",
+	"couldNotMoveDirectory"        => "could not move directory \"%1\" to \"%2\"",
+	"couldNotCreateFile"           => "could not write file \"%1\"",
+	"errorSettingFileContent"      => "error setting file content",
+	"failedCreateDirectoryCheck"   => "failed to create directory \"%1\"\ncheck filesystem permissions",
+	"failedRenameDirectory"        => "failed to rename directory",
+	"failedToSaveTmpFile"          => "failure to save tmp file \"%1\" to location \"%2\"",
+	"fileAlreadyExists"            => "a file of that name already exists",
+	"filesCopied"                  => "%1 files copied",
+	"illegalDirectoryName"         => "\"%1\" is an illegal directory name",
+	"illegalFileName"              => "\"%1\" is an illegal file name",
+	"illegalTargetDirectory"       => "illegal target directory \"%1\"",
+	"isNotWritable"                => "\"%1\" is not writable",
+	"missingFileInSelection"       => "missing file in selection",
+	"noDataForFileID"              => "no data for file id \"%1\"",
+	"noDataForDirectoryID"         => "no data for directory id \"%1\"",
+	"noNativeZipCommand"           => "no \"zip\" command found on server",
+	"permissionDeniedUpload"       => "permission denied for upload to this directory",
+	"permissionDeniedCreateDirectory"=>"permission denied: cannot create directory",
+	"permissionDeniedCreateFile"   => "permission denied: cannot create file",
+	"permissionDeniedDeleteDirectory"=>"permission denied: cannot edit directory",
+	"permissionDeniedDeleteFile"   => "permission denied: cannot delete file",
+	"permissionDeniedEditDirectory"=> "permission denied: cannot edit directory",
+	"permissionDeniedEditFile"     => "permission denied: cannot edit file",
+	"permissionDeniedMoveDirectory"=> "permission denied: cannot move directory",
+	"permissionDeniedMoveFile"     => "permission denied: cannot move file",
+	"permissionDeniedRename"       => "permission denied: cannot rename \"%1\"",
+	"permissionDeniedViewFile"     => "permission denied: cannot view file",
+	"searchResults"                => "search results"
+);
+?>
