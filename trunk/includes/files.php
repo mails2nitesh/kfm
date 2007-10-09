7cfbad044c2c5a42eba728240442d313867ae6b6
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-10-09 14:05:01 +0000

    move $kfm_banned_files to configuration

--- trunk/includes/files.php
+++ trunk/includes/files.php
@@ -285,7 +285,6 @@ function _search($keywords,$tags){
 			if(!$file->checkName())continue;
 			if($file->isImage())$file=kfmImage::getInstance($f['id']);
 			unset($file->db);
-			unset($file->db);
 			$files[]=$file;
 		}
 	}
