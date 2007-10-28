cc430ad5c30e4494108b1eef00a3bbd02bd9daa0
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-10-28 13:34:45 +0000

    create branches and tags directory

---
+++ branches/bterkuile/lang/de.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * See ../license.txt for licensing
+ *
+ * For further information visit:
+ *  http://kfm.verens.com/
+ *
+ * File Name: en.php
+ *  English language file.
+ *
+ * File Authors:
+ *  kae@verens.com
+ */
+
+$kfm_langStrings=array(
+	"alreadyExists"                => "\"%1\" existiert bereits",
+	"errorInvalidID"               => "Error: unzulässige ID",
+	"errorFileIDNotFound"          => "Error: die Datei ID #%1 wurde in der Datenbank nicht gefunden",
+	"bannedFilenameExtension"      => "verbotene Dateinamenerweiterung",
+	"cannotEditRestrictedExtension"=> "konnte \"%1\" nicht bearbeiten\nwegen Dateierweiterungsbeschrenkung",
+	"cannotMoveIntoSelf"           => "ich kann ein Verzeichniss nicht in sein eigenes Unterverzeichnis verschieben",
+	"cannotRenameFromTo"           => "kann \"%1\" nicht zu  \"%2\" umbenennen",
+	"CMSRefusesFileDelete"         => "CMS erlaubt es nicht \"%1\" zu löschen",
+	"couldNotMoveDirectory"        => "konnte das Verzeichnis \"%1\" nicht nach \"%2\" verschieben",
+	"couldNotCreateFile"           => "konnte die Datei \"%1\" nicht erstellen",
+	"errorSettingFileContent"      => "Fehler beim erstellen des Dateiinhalts",
+	"failedCreateDirectoryCheck"   => "Fehler beim Erstellen des Verzeichnisses \"%1\"\nÜberprüfen Sie die Zugriffsrechte",
+	"failedRenameDirectory"        => "Fehler beim Umbenennen des Verzeichnisses",
+	"failedToSaveTmpFile"          => "Fehler beim speichern der tmp Datei \"%1\" nach \"%2\"",
+	"fileAlreadyExists"            => "eine Datei mit diesem Namen existiert bereis",
+	"filesCopied"                  => "%1 Dateien kopiert",
+	"illegalDirectoryName"         => "\"%1\" ist ein illegaler Verzeichnisname",
+	"illegalFileName"              => "\"%1\" ist ein illegaler Dateiname",
+	"illegalTargetDirectory"       => "illegales Zielverzeichnis \"%1\"",
+	"isNotWritable"                => "\"%1\" ist nicht Schreibbar",
+	"missingFileInSelection"       => "Verlorene Datei in der Auswahl",
+	"noDataForFileID"              => "keine Daten für Datei-ID \"%1\"",
+	"noDataForDirectoryID"         => "keine Daten für Verzeichnis-ID \"%1\"",
+	"noNativeZipCommand"           => "kein \"zip\" Commando auf dem Server gefunden",
+	"permissionDeniedUpload"       => "Zugriff verweigert für das Hochladen in dieses Verzeichnis",
+	"permissionDeniedCreateDirectory"=>"Zugriff verweigert: kann das Verzeichnis nicht erstellen",
+	"permissionDeniedCreateFile"   => "Zugriff verweigert: kann die Datei nicht erstellen",
+	"permissionDeniedDeleteDirectory"=>"Zugriff verweigert: kann das Verzeichnis nicht löschen",
+	"permissionDeniedDeleteFile"   => "Zugriff verweigert: kann die Datei nicht löschen",
+	"permissionDeniedEditDirectory"=> "Zugriff verweigert: kann das Verzeichnis nicht bearbeiten",
+	"permissionDeniedEditFile"     => "Zugriff verweigert: kann die Datei nicht bearbeiten",
+	"permissionDeniedMoveDirectory"=> "Zugriff verweigert: kann das Verzeichnis nicht verschieben",
+	"permissionDeniedMoveFile"     => "Zugriff verweigert: kann die Datei nicht verschieben",
+	"permissionDeniedRename"       => "Zugriff verweigert: kann \"%1\" nicht umbenennen",
+	"permissionDeniedViewFile"     => "Zugriff verweigert: kann die Datei nicht anzeigen",
+	"searchResults"                => "Suchergebnisse",
+	# 1.2
+	"fileNotCreatedDirUnwritable"    => "\"%1\" could not be created. The directory is not writable",
+	"fileNotDeletedUnwritable"       => "\"%1\" cannot be removed. (file not writable)",
+	"fileNotMovableUnwritable"       => "\"%1\" cannot be moved. (file not writable)",
+	"permissionDeniedManipImage"     => "permission denied: cannot manipulate images",
+	"imageNotWritable"               => "image is not writable",
+	"failedGetFileObject"            => "failed to retrieve File object",
+	"failedGetDirectoryObject"       => "failed to retrieve Directory object",
+	"failedDeleteFile"               => "failed to delete file \"%1\"",
+	"failedMoveFile"                 => "failed to move file \"%1\"",
+	"failedWriteToFile"              => "failed write to file \"%1\"",
+	"failedDownloadFromUrl"        => "failed to download from URL \"%1\""
+);
+?>
---
+++ branches/kae/lang/nl.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * See ../license.txt for licensing
+ *
+ * For further information visit:
+ *  http://kfm.verens.com/
+ *
+ * File Name: nl.php
+ *  Dutch language file.
+ *
+ * File Authors:
+ *  Roy Lubbers (roy@cns-it.nl)
+ */
+
+$kfm_langStrings=array(
+	"alreadyExists"                => "\"%1\" bestaat al",
+	"errorInvalidID"               => "fout: ongeldige ID",
+	"errorFileIDNotFound"          => "fout: bestands-id #%1 niet gevonden in database",
+	"bannedFilenameExtension"      => "verbannen extensie in bestandsnaam",
+	"cannotEditRestrictedExtension"=> "kan \"%1\" niet bewerken\nrestrictie op bestandsextensie",
+	"cannotMoveIntoSelf"           => "kan map niet verplaatsen naar eigen submap",
+	"cannotRenameFromTo"           => "kan \"%1\" niet hernoemen naar \"%2\"",
+	"CMSRefusesFileDelete"         => "CMS staat niet toe dat \"%1\" verwijderd wordt",
+	"couldNotMoveDirectory"        => "kan map \"%1\" niet verplaatsen naar \"%2\"",
+	"couldNotCreateFile"           => "kan bestand \"%1\" niet schrijven",
+	"errorSettingFileContent"      => "fout aanpassen bestandsinhoud",
+	"failedCreateDirectoryCheck"   => "kan map \"%1\" niet maken\ncontroleer bestandssysteempermissies",
+	"failedRenameDirectory"        => "kan map niet hernoemen",
+	"failedToSaveTmpFile"          => "kan tmp-bestand \"%1\" niet opslaan op locatie \"%2\"",
+	"fileAlreadyExists"            => "een bestand met dezelfde naam bestaat al",
+	"filesCopied"                  => "%1 bestanden gekopieerd",
+	"illegalDirectoryName"         => "\"%1\" is een ongeldige mapnaam",
+	"illegalFileName"              => "\"%1\" is een ongeldige bestandsnaam",
+	"illegalTargetDirectory"       => "ongeldige doelmap \"%1\"",
+	"isNotWritable"                => "\"%1\" is niet schrijfbaar",
+	"missingFileInSelection"       => "mis bestand in selectie",
+	"noDataForFileID"              => "geen gegevens voor bestands-id \"%1\"",
+	"noDataForDirectoryID"         => "geen gegeven voor map-id \"%1\"",
+	"noNativeZipCommand"           => "geen \"zip\" commando gevonden op de server",
+	"permissionDeniedUpload"       => "toegang geweigerd voor uploaden naar deze map",
+	"permissionDeniedCreateDirectory"=>"toegang geweigerd: kan map niet maken",
+	"permissionDeniedCreateFile"   => "toegang geweigerd: kan bestand niet maken",
+	"permissionDeniedDeleteDirectory"=>"toegang geweigerd: kan map niet verwijderen",
+	"permissionDeniedDeleteFile"   => "toegang geweigerd: kan bestand niet verwijderen",
+	"permissionDeniedEditDirectory"=> "toegang geweigerd: kan map niet bewerken",
+	"permissionDeniedEditFile"     => "toegang geweigerd: kan bestand niet bewerken",
+	"permissionDeniedMoveDirectory"=> "toegang geweigerd: kan map niet verplaatsen",
+	"permissionDeniedMoveFile"     => "toegang geweigerd: kan bestand niet verplaatsen",
+	"permissionDeniedRename"       => "toegang geweigerd: kan \"%1\" niet hernoemen",
+	"permissionDeniedViewFile"     => "toegang geweigerd: kan bestand niet bekijken",
+	"searchResults"                => "zoekresultaten",
+	# 1.2
+	"fileNotCreatedDirUnwritable"    => "\"%1\" could not be created. The directory is not writable",
+	"fileNotDeletedUnwritable"       => "\"%1\" cannot be removed. (file not writable)",
+	"fileNotMovableUnwritable"       => "\"%1\" cannot be moved. (file not writable)",
+	"permissionDeniedManipImage"     => "permission denied: cannot manipulate images",
+	"imageNotWritable"               => "image is not writable",
+	"failedGetFileObject"            => "failed to retrieve File object",
+	"failedGetDirectoryObject"       => "failed to retrieve Directory object",
+	"failedDeleteFile"               => "failed to delete file \"%1\"",
+	"failedMoveFile"                 => "failed to move file \"%1\"",
+	"failedWriteToFile"              => "failed write to file \"%1\"",
+	"failedDownloadFromUrl"        => "failed to download from URL \"%1\""
+);
+?>
