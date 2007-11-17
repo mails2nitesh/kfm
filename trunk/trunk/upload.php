ed6b336df6da60bd0fd05fb107f74b7d826a460e
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2007-11-17 06:59:23 +0000

    remove references to kfm_add_file_to_db() (it was converted to OO a while back)

--- trunk/trunk/upload.php
+++ trunk/trunk/upload.php
@@ -9,7 +9,7 @@ if($kfm_allow_file_upload){
 	$toDir=kfmDirectory::getInstance($kfm_session->get('cwd_id'));
 	$to=$toDir->path.'/'.$filename;
 	if(!kfm_checkAddr($to))$errors[]=kfm_lang('bannedFilenameExtension');
-	elseif(!kfmFile::checkName($filename))$errors[]='The filename: '.$filename.' is not allowed';
+	else if(!kfmFile::checkName($filename))$errors[]='The filename: '.$filename.' is not allowed';
 	else{
 		move_uploaded_file($tmpname,$to);
 		if(!file_exists($to))$errors[]=kfm_lang('failedToSaveTmpFile',$tmpname,$to);
@@ -19,7 +19,7 @@ if($kfm_allow_file_upload){
 		}
 		else{
 			chmod($to, octdec('0'.$kfm_default_upload_permission));
-			$fid=kfm_add_file_to_db($filename,$kfm_session->get('cwd_id'));
+			$fid=kfmFile::addToDb($filename,$kfm_session->get('cwd_id'));
 			$file=kfmFile::getInstance($fid);
 			if(function_exists('exif_imagetype')){
 				$imgtype=@exif_imagetype($to);
@@ -61,7 +61,7 @@ else $errors[]=kfm_lang('permissionDeniedUpload');
 $js=isset($_REQUEST['js'])?$js:'';
 if(isset($_REQUEST['onload']))echo $_REQUEST['onload'];
 else if(isset($_REQUEST['onupload']))echo $_REQUEST['onupload'];
-else if(count($errors))echo 'alert("'.addslashes(join("\n",$errors)).'")';
+else if(count($errors))echo 'alert("'.addslashes(join("\n",$errors)).'");';
 else echo 'parent.x_kfm_loadFiles('.$kfm_session->get('cwd_id').',parent.kfm_refreshFiles);parent.kfm_dir_openNode('.$kfm_session->get('cwd_id').');'.$js;
 ?>
 		</script>
