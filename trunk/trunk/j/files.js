dca3f8e3b31e84babb228e1b44db3769bd743f4a
bterkuile <bterkuile@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2008-01-04 12:43:24 +0000

    second attempt sorting by category, fixed the trash widget problem (cause, reference to selectedFiles, no hard copy)

--- trunk/trunk/j/files.js
+++ trunk/trunk/j/files.js
@@ -79,8 +79,10 @@ function kfm_deleteSelectedFiles(){
     if(!kfm_vars.permissions.file.rm)return kfm.alert('permission denied: cannot delete files');
     kfm_deleteFiles(selectedFiles);
 }
-function kfm_deleteFiles(dfiles){
+function kfm_deleteFiles(files){
     var names=[],m='';
+	 var dfiles=[]; // break reference system
+	 for(var j=0;j<files.length;j++)dfiles.push(files[j]);//breaking reference to selectedFiles
     if(dfiles.length>10){
         for(var i=0;i<9;++i)names.push(File_getInstance(dfiles[i]).name);
         m='\n'+kfm.lang.AndNMore(dfiles.length-9);
