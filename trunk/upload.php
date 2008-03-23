78da21d01042c4b5896e881004b0dc28fdff4ccc
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2008-03-23 09:29:20 +0000

    run files through phpcs, make code more compliant with Pear syntax

--- trunk/upload.php
+++ trunk/upload.php
@@ -12,51 +12,51 @@
  * @link     http://kfm.verens.com/
  */
 require 'initialise.php';
-$errors=array();
+$errors = array();
 if ($kfm_allow_file_upload) {
-   $file=isset($_FILES['kfm_file'])?$_FILES['kfm_file']:$_FILES['Filedata'];
-   $filename=$file['name'];
-   $tmpname=$file['tmp_name'];
-	$cwd=$kfm_session->get('cwd_id');
-	if(!$cwd) $errors[]=kfm_lang('CWD not set');
-	else {
-		$toDir=kfmDirectory::getInstance($cwd);
-		$to=$toDir->path.'/'.$filename;
-		if (!is_file($tmpname)) $errors[]='No file uploaded';
-	   else if (!kfmFile::checkName($filename)){
-			$errors[]='The filename: '.$filename.' is not allowed';
-		}
-	}
-	if(file_exists($to))$errors[]='File already exists'; #TODO new string
-	if(!count($errors)) {
-		move_uploaded_file($tmpname, $to);
-      if (!file_exists($to)) $errors[]=kfm_lang('failedToSaveTmpFile', $tmpname, $to);
-      else if ($kfm_only_allow_image_upload && !getimagesize($to)) {
-			$errors[]='only images may be uploaded';
-			unlink($to);
-      } else {
+    $file     = isset($_FILES['kfm_file'])?$_FILES['kfm_file']:$_FILES['Filedata'];
+    $filename = $file['name'];
+    $tmpname  = $file['tmp_name'];
+    $cwd      = $kfm_session->get('cwd_id');
+    if(!$cwd) $errors[] = kfm_lang('CWD not set');
+    else {
+        $toDir = kfmDirectory::getInstance($cwd);
+        $to = $toDir->path.'/'.$filename;
+        if (!is_file($tmpname)) $errors[] = 'No file uploaded';
+        else if (!kfmFile::checkName($filename)) {
+            $errors[] = 'The filename: '.$filename.' is not allowed';
+        }
+    }
+    if (file_exists($to)) $errors[] = 'File already exists'; // TODO new string
+    if (!count($errors)) {
+        move_uploaded_file($tmpname, $to);
+        if (!file_exists($to)) $errors[] = kfm_lang('failedToSaveTmpFile', $tmpname, $to);
+        else if ($kfm_only_allow_image_upload && !getimagesize($to)) {
+            $errors[] = 'only images may be uploaded';
+            unlink($to);
+        } else {
             chmod($to, octdec('0'.$kfm_default_upload_permission));
-            $fid=kfmFile::addToDb($filename, $kfm_session->get('cwd_id'));
-            $file=kfmFile::getInstance($fid);
+            $fid  = kfmFile::addToDb($filename, $kfm_session->get('cwd_id'));
+            $file = kfmFile::getInstance($fid);
             if (function_exists('exif_imagetype')) {
-                $imgtype=@exif_imagetype($to);
+                $imgtype = @exif_imagetype($to);
                 if ($imgtype) {
-                    $file=kfmImage::getInstance($file);
-                    $comment='';
+                    $file    = kfmImage::getInstance($file);
+                    $comment = '';
                     if ($imgtype==1) { // gif
-                        $fc=file_get_contents($to);
-                        $arr=explode('!', $fc);
-                        $found=0;
-                        for ($i=0;$i<count($arr)&&!$found;++$i) {
-                            $block=$arr[$i];
+                        $fc    = file_get_contents($to);
+                        $arr   = explode('!', $fc);
+                        $found = 0;
+                        for ($i = 0;$i<count($arr)&&!$found;++$i) {
+                            $block = $arr[$i];
                             if (substr($block, 0, 2)==chr(254).chr(21)) {
-                                $found=1;
-                                $comment=substr($block, 2, strpos($block, 0)-1);
+                                $found   = 1;
+                                $comment = substr($block, 2, strpos($block, 0)-1);
                             }
                         }
                     } else {
-                        $data=exif_read_data($to, 0, true);
-                        if (is_array($data)&&isset($data['COMMENT'])&&is_array($data['COMMENT'])) $comment=join("\n", $data['COMMENT']);
+                        $data = exif_read_data($to, 0, true);
+                        if (is_array($data)&&isset($data['COMMENT'])&&is_array($data['COMMENT'])) $comment = join("\n", $data['COMMENT']);
                     }
                     $file->setCaption($comment);
                 } else if (isset($_POST['kfm_unzipWhenUploaded'])&&$_POST['kfm_unzipWhenUploaded']) {
@@ -66,18 +66,18 @@ if ($kfm_allow_file_upload) {
             }
         }
     }
-} else $errors[]=kfm_lang('permissionDeniedUpload');
-if(isset($_REQUEST['swf']) && $_REQUEST['swf']==1){
-	if(count($errors))echo join("\n",$errors);
-	else echo 'OK';
-	exit;
+} else $errors[] = kfm_lang('permissionDeniedUpload');
+if (isset($_REQUEST['swf']) && $_REQUEST['swf']==1) {
+    if(count($errors))echo join("\n", $errors);
+    else echo 'OK';
+    exit;
 }
 ?>
 <html>
     <head>
         <script type="text/javascript">
 <?php
-$js=isset($_REQUEST['js'])?$js:'';
+$js = isset($_REQUEST['js'])?$js:'';
 if (isset($_REQUEST['onload'])) echo $_REQUEST['onload'];
 else if (isset($_REQUEST['onupload'])) echo $_REQUEST['onupload'];
 else if (count($errors)) echo 'alert("'.addslashes(join("\n", $errors)).'");';
