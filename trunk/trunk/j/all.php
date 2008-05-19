202901e1036ccb4ebafbf90b23f3becae27765dd
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2008-05-19 10:11:21 +0000

    push javascript through minifier before sending to client.

--- trunk/trunk/j/all.php
+++ trunk/trunk/j/all.php
@@ -19,6 +19,9 @@ $js.=file_get_contents('file.class.js');
 $js.=file_get_contents('files.js');
 $js.=file_get_contents('resize_handler.js');
 $js.=file_get_contents('search.js');
+
 header('Content-type: text/javascript');
 header('Expires: '.gmdate("D, d M Y H:i:s", time() + 3600*24*365).' GMT');
-echo $js;
+
+require '../includes/jsmin-1.1.1.php';
+echo JSMin::minify($js);
--- trunk/trunk/j/jquery/all.php
+++ trunk/trunk/j/jquery/all.php
@@ -7,6 +7,9 @@ $js.=file_get_contents('jquery.idrag.js');
 $js.=file_get_contents('jquery.grid.columnSizing.js');
 //$js.=file_get_contents('jquery.tablesorter.pack.js');
 $js.=file_get_contents('jquery.tablesorter.js');
+
 header ('Content-type: text/javascript');
 header('Expires: '.gmdate("D, d M Y H:i:s", time() + 3600*24*365).' GMT');
-echo $js;
+
+require '../../includes/jsmin-1.1.1.php';
+echo JSMin::minify($js);
