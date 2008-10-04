04339ac2508a0c287410f522104b2914e23feb72
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2008-10-04 17:18:51 +0000

    fix pgsql in 1.4 updates and creation scripts

--- trunk/trunk/scripts/db.pgsql.update.1.4.php
+++ trunk/trunk/scripts/db.pgsql.update.1.4.php
@@ -3,22 +3,22 @@ $kfmdb->query("CREATE TABLE ".KFM_DB_PREFIX."users(
 	id serial,
 	username varchar(16),
 	password varchar(40),
-	status INTEGER(1) default 2
+	status INTEGER default 2
 )");
  
 $kfmdb->query("CREATE TABLE ".KFM_DB_PREFIX."settings(
 	id serial,
 	name varchar(128),
 	value varchar(256),
-	user_id INTEGER(8),
-	usersetting INTEGER(1) default 0
+	user_id INTEGER,
+	usersetting INTEGER default 0
 )");
 
 $kfmdb->query("CREATE TABLE ".KFM_DB_PREFIX."plugin_extensions(
 	id serial,
 	extension varchar(64),
 	plugin varchar(64),
-	user_id INTEGER(8)
+	user_id INTEGER
 )");
 
 $kfmdb->query('INSERT INTO '.KFM_DB_PREFIX.'users (id, username, password, status) VALUES (1,"admin", "'.sha1('admin').'",1)');
