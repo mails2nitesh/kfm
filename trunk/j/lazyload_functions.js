97acc7f410e89de82cac9a67dee467d385141a49
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2008-11-09 21:59:09 +0000

    some more functions converted to lazyload versions.
    a few more notes:
     1. don't convert rapidly regenerating functions to lazyload. for example, an "ondrag" function might be called many times before the javascript finally generates
     2. if you have something like $j(blah).click(ll_func), you'll need to convert to $j(blah).click(function(vars){ll_func(vars);}); (the first version copies the function literally - doesn't create a pointer)

--- trunk/j/lazyload_functions.js
+++ trunk/j/lazyload_functions.js
@@ -8,14 +8,14 @@
  *
  */
 
+function lazyload_replace_stub(fname,js,ps){ // replace stub with function, then call function with original parameters
+	eval(js);
+	(eval(fname))(ps[0],ps[1],ps[2],ps[3]); // hacky method... replace when a better idea comes along
+}
+
 var i,funcs=[],fname;
 for(i=0;i<llStubs.length;++i){
 	fname=llStubs[i];
 	funcs.push('window.'+fname+'=function(){var ps=arguments;x_kfm_getJsFunction("'+fname+'",function(js){lazyload_replace_stub("'+fname+'",js,ps);});};');
 }
 eval(funcs.join("\n"));
-
-function lazyload_replace_stub(fname,js,ps){ // replace stub with function, then call function with original parameters
-	eval(js);
-	(eval(fname))(ps[0],ps[1],ps[2],ps[3]); // hacky method... replace when a better idea comes along
-}
--- trunk/j/search.js
+++ trunk/j/search.js
@@ -2,17 +2,10 @@
 function kfm_runSearch(){
 	kfm_run_delayed('search',kfm_runSearch2);
 }
-function kfm_runSearch2(){
-	var keywords='',tags='';
-	var kEl=document.getElementById("kfm_search_keywords"),tEl=document.getElementById("kfm_search_tags");
-	if(kEl)keywords=kEl.value;
-	if(tEl)tags=tEl.value;
-	if(keywords==""&&tags=="")x_kfm_loadFiles(kfm_cwd_id,kfm_refreshFiles);
-	else x_kfm_search(keywords,tags,kfm_refreshFiles)
-}
 function kfm_searchBoxFile(){
 	var sbox=document.createElement('input');
 	sbox.id='kfm_search_keywords';
 	$j.event.add(sbox,'keyup',kfm_runSearch);
 	return sbox;
 }
+llStubs.push('kfm_runSearch2');
