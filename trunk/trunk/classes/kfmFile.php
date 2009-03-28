5889503b03d65f3244c14da36acab9db4521312d
bterkuile <bterkuile@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2009-03-28 10:24:56 +0000

    chagen suggested by joss.eve in the forum

--- trunk/trunk/classes/kfmFile.php
+++ trunk/trunk/classes/kfmFile.php
@@ -65,12 +65,33 @@ class kfmFile extends kfmObject{
 		return $this->exists;
 	}
 
-	/**
-	 * Returns the file contents or false on error
-	 */
-	function getContent(){
-		return ($this->id==-1)?false:utf8_encode(file_get_contents($this->path));
-	}
+   /**
+   * Returns true if $string is valid UTF-8 and false otherwise.
+   *
+   * @since        1.14
+   * @param [mixed] $string     string to be tested
+   * @subpackage
+   */
+   function is_utf8($string) {
+      // From http://w3.org/International/questions/qa-forms-utf-8.html
+      return preg_match('%^(?:
+            [\x09\x0A\x0D\x20-\x7E]            # ASCII
+          | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+          |  \xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+          | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+          |  \xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+          |  \xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+          | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+          |  \xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+      )*$%xs', $string);
+   }
+
+   /**
+    * Returns the file contents or false on error
+    */
+   function getContent(){
+      return ($this->id == -1) ? false : $this->is_utf8($this->path) ? file_get_contents($this->path) : utf8_encode(file_get_contents($this->path));
+   }
 
 	/**
 	 * Function that returns the extension of the file.
