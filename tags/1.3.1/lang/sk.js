fa65ea2bc0c481152a0177d356baa40559e5b357
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2009-06-26 07:54:55 +0000

    add Slovak translation

---
+++ tags/1.3.1/lang/sk.js
@@ -0,0 +1,157 @@
+/*
+ * See ../license.txt for licensing
+ *
+ * For further information visit:
+ * 	http://kfm.verens.com/
+ *
+ * File Name: sk.js
+ * 	Slovak language file.
+ *
+ * File Authors:
+ * 	Peter Beňo <najlepsiwebdesigner@gmail.com>
+ */
+
+kfm.lang={
+AddTagsToFiles: "pridať tagy súboru/súborom",
+AllowedFileExtensions: "povolené koncovky súborov",
+AreYouSureYouWantToCloseKFM: "Naozaj chcete zatvoriť okno?",
+AskIfOverwrite: "Subor s názvom \"%1\" už existuje. Prepísať?",
+CouldNotWriteFile: "chyba: nieje možné zapísať súbor \"%1\".",
+CreateSubDir: "vytvoriť podadresár",
+CurrentWorkingDir: "Nachádzate sa v adresári: \"%1\"",
+CouldNotRemoveDir: "nieje možné zmazať adresár.\nuistite sa, že je prázdny",
+CouldNotDownloadFile: "chyba: nieje možné stiahnuť súbor \"%1\".",
+CouldntReadDir: "chyba: nieje možné prečítať adresár",
+CreateEmptyFile: "vytvoriť prázdny súbor",
+CloseWithoutSavingQuestion: "Zavrieť bez uloženia?",
+CloseWithoutSaving: "Zavrieť bez uloženia.",
+CannotRenameFile: "chyba: nieje možné premenovať \"%1\" na \"%2\"",
+ChangeCaption: "zmeniť popisok",
+CreateDirMessage: "Vytvoriť podadresár adresára \"%1\":",
+CurrentSize: "Veľkosť: \"%1\" x \"%2\"\n",
+ChangeCaption: "Zmeniť popisok",
+Close: "Zavrieť",
+Caption: "Popisok",
+CommaSeparated: "oddelené čiarkou",
+CopyFromURL: "Skopírovať z url",
+Dir: "ltr", // language direction
+Directories: "Adresáre",
+DeleteFile: "zmazať",
+DirEmpty: "adresár \"%1\" neobsahuje žiadne súbory",
+DeleteDir: "zmazať",
+DelDirMessage: "Naozaj chcete zmazať adresár \"%1\"?",
+DelFileMessage: "Naozaj chcete zmazať súbor \"%1\"",
+DelMultipleFilesMessage: "Naozaj chcete zmazať tieto súbory?\n\n",
+DownloadFileFromMessage: "Odkiaľ stiahnuť súbor?",
+DirRefreshed: "zoznam adresárov bol obnovený.",
+DirectoryNameExists: "adresár s týmto menom už existuje.",
+DownloadFileFromUrl: "Stiahnuť súbor z url",
+DirectoryProperties: "Vlastnosti",
+DirNotInDb: "adresár nieje v databáze",
+ErrorPrefix: "chyba: ",
+EditTextFile: "upraviť textový súbor",
+ExtractZippedFile: "Rozbaliť",
+FileSavedAsMessage: "Uložiť ako?",
+FileUpload: "Nahrávanie súborov na server",
+FilesRefreshed: "zoznam súborov bol obnovený.",
+FileSelected: "vybraný subor: \"%1\"",
+FileNameNotAllowd: "chyba: nepovolený názov súboru",
+FileTooLargeForThumb: "chyba: \"%1\" je má príliš veľké rozmery, nahrajte menšiu verziu.",
+FilenameAlreadyExists: "chyba: súbor s týmto názvom už existuje",
+FileDetails: "Detaily súboru",
+Filename: "názov súboru",
+Filesize: "velkosť súboru",
+HowWouldYouLikeToRenameTheseFiles: "Ako chcete premenovať súbory?\n\npríklad: \"images-***.jpg\" budú premenované na \"images-001.jpg\", \"images-002.jpg\", ...",
+InvertSelection: "invertovať výber",
+IfYouUseMultipleWildcards: "Pokiaľ použijete viacero značiek pre výber súborov, musia byť spojené dohromady.",
+IllegalDirectoryName: "neplatný názov adresára \"%1\"",
+LockPanels: "zamknúť panely",
+Loading: "nahrávam",
+LoadingKFM: "nahrávam KFM",
+Log_ChangeCaption: "zmenený popisok \"%1\" na \"%2\"",
+Logs: "Logy",
+MovingFilesTo: "presúvam súbory [\"%1\"] do \"%2\"",
+Maximise: "maximalizovať",
+Minimise: "minimalizovať",
+MoveDown: "presunúť dole",
+Mimetype: "MIME typ",
+MoveUp: "presunúť hore",
+NewCaptionIsThisCorrect: "Nový popisok:\n%1\n\nOk?",
+Name: "názov",
+NewDirectory: "Nový adresár",
+NoRestrictions: "bez obmedzení",
+NoForwardslash: "\nNemôžete použiť '/' v názve souboru",
+NewWidth: "Nová šírka?",
+NoPanel: "chyba: panel \"%1\" neexistuje.",
+NewCaption: "Nový popisok",
+NewWidthConfirmTxt: "Nová šírka: \"%1\"\n",
+NewHeight: "Nová výška?",
+NewHeightConfirmTxt: "Nová výška: \"%1\"\n\nOk?",
+NotMoreThanOneFile: "chyba: nieje možné vybrať viacero súborov naraz",
+NoFilesSelected: "neboli vybrané žiadne súbory",
+PleaseSelectFileBeforeRename: "Je potrebné vybrať súbor",
+RenameDir: "premenovať",
+RenameFile: "premenovať",
+RotateClockwise: "otočiť v smere hod.ručičiek",
+RotateAntiClockwise: "otočiť proti smeru hod. ručičiek",
+ResizeImage: "zmeniť veľkosť obrázku",
+RenamedFile: "premenovávam súbor \"%1\" na \"%2\".",
+Restore: "obnoviť",
+ReturnThumbnailToOpener: "späť k editácii",
+RenameOnlyOneFile: "Premenovať môžete iba jeden súbor",
+RecursiveDeleteWarning: "Adresár \"%1\" nieje prázdny\nChcete ho naozaj zmazať vrátane obsahu?\n*POZOR* TÁTO ZMENA JE NEVRATNÁ",
+RmdirFailed: "chyba pri mazání \"%1\"",
+RenameFileToWhat: "Premenovať \"%1\" na?",
+RemoveTagsFromFiles: "odstraniť tagy zo súboru/súborov",
+SetStyleschyba: "chyba: nieje možné nastaviť \"%1\" na \"%2\.",
+SaveThenClose: "Uložiť a zavrieť",
+SaveThenCloseQuestion: "Naozaj uložiť všetky zmeny?",
+SelectAll: "vybrať všetko",
+SelectNone: "zrušiť výber",
+Search: "Hľadať",
+ShowPanel: "ukázať panel \"%1\"",
+Tags: "tagy",
+UnknownPanelState: "chyba: neznámy stav panelu.",
+UrlNotValidLog: "chyba: URL musí začínať s \"http:\"",
+UnlockPanels: "odomknúť panely",
+UrlNotValid: "chyba: URL musí začínať s \"http:\"",
+Upload: "Nahrať",
+ViewImage: "zobraziť obrázok",
+WhatFilenameToCreateAs: "Názov nového súboru?",
+WhatMaximumSize: "Maximálna veľkosť?",
+WhatIsTheNewTag: "Zadajte názov nového tagu.\nViacero tagov oddeľte čiarkou.",
+WhichTagsDoYouWantToRemove: "Ktoré tagy chcete odstrániť?\nViacero tagov oddeľte čiarkou.",
+YouMustPlaceTheWildcard: "Do hľadaného mena súboru musíte umiestniť znak *",
+YouNeedMoreThan: "Potrebujete viac než %1 * znakov pre vytvorenie %2 názvu súboru"
+
+,
+// New in 0.9
+AllFiles: "všetky súbory",
+AndNMore: "...a o %1 viac...",
+Browse: "Prechádzať...",
+ExtractAfterUpload: "rozbaliť po nahraní na server",
+NotAnImageOrImageDimensionsNotReported: "chyba: nieje obrázok alebo niesú zadané rozmery obrázka",
+PermissionDeniedCannotDeleteFile: "prístup odoprený: nieje možné zmazať súbor",
+RenameTheDirectoryToWhat: "Premenovať adresár na '%1'?",
+RenamedDirectoryAs: "Premenovaný '%1' na '%2'",
+TheFilenameShouldEndWithN: "Názov súboru by mal končiť %1",
+WhatFilenameDoYouWantToUse: "Aký názov súboru chcete použiť?"
+
+,
+// New in 1.0
+ZipUpFiles: "zazipovať súbory",
+Cancel: "Zrušiť"
+	, // new in 1.2
+	Icons                   : "ikony", // used to select mode of file view
+	ListView                : "zoznam", // used to select mode of file view
+	SendToCms               : "odoslať do CMS", // close KFM and return the selected files to the CMS
+	CannotMoveDirectory     : "prístup odoprený: nieje možné presunúť adresár",
+	LastModified            : "naposledy upravený", // part of File Details
+	ImageDimensions         : "rozmery", // part of File Details
+	CouldNotMoveFiles       : "error: nieje možné presunúť súbor/súbory",
+	CopyFiles               : "kopírovať súbory", // when dragging files to a directory, two choices appear - "copy files" and "move files"
+	MoveFiles               : "presunúť súbory",
+	AboutKfm                : "o KFM",
+	Errors                  : "Chyby",
+	Ok                      : "OK" // as in "OK / Cancel"
+}
---
+++ tags/1.3.1/lang/sk.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * See ../license.txt for licensing
+ *
+ * For further information visit:
+ *  http://kfm.verens.com/
+ *  Prvá verzia prekladu
+ *  This is the first slovak translation, check for updates later. 
+ * File Name: sk.php
+ *  Slovak language file.
+ *
+ * File Authors:
+ *  kae@verens.com
+ *  najlepsiwebdesigner@gmail.com
+ */
+
+$kfm_langStrings=array(
+	"alreadyExists"                => "\"%1\" už existuje",
+	"errorInvalidID"               => "chyba: neplatné ID",
+	"errorFileIDNotFound"          => "chyba: súbor id #%1 sa nenachádza v databáze",
+	"bannedFilenameExtension"      => "nepovolená koncovka súboru",
+	"cannotEditRestrictedExtension"=> "nieje možné upravovať \"%1\"\n, zakázaná prípona súboru",
+	"cannotMoveIntoSelf"           => "nieje možné presunúť do samého seba",
+	"cannotRenameFromTo"           => "nieje možné premenovať \"%1\" na \"%2\"",
+	"CMSRefusesFileDelete"         => "Systém nepovoľuje zmazanie súboru/adresára \"%1\" ",
+	"couldNotMoveDirectory"        => "nieje možné presunúť adresár \"%1\" do \"%2\"",
+	"couldNotCreateFile"           => "nieje možné zapísať súbor \"%1\"",
+	"errorSettingFileContent"      => "nieje možné nastaviť obsah súboru",
+	"failedCreateDirectoryCheck"   => "nieje možné vytvoriť adresár \"%1\"\nskontrolujte práva",
+	"failedRenameDirectory"        => "nieje možné premenovať adresár",
+	"failedToSaveTmpFile"          => "nieje možné uložiť dočasný súbor \"%1\" do \"%2\"",
+	"fileAlreadyExists"            => "súbor s týmto názvom už existuje",
+	"filesCopied"                  => "%1 súborov bolo skopírovaných",
+	"illegalDirectoryName"         => "\"%1\" je neplatné meno adresára",
+	"illegalFileName"              => "\"%1\" je neplatné meno súboru",
+	"illegalTargetDirectory"       => "neplatný cieľový adresár \"%1\"",
+	"isNotWritable"                => "\"%1\" nieje určený k zápisu",
+	"missingFileInSelection"       => "vo výbere chýba súbor",
+	"noDataForFileID"              => "žiadne dáta pre súbor id \"%1\"",
+	"noDataForDirectoryID"         => "žiadne dáta pre adresár id \"%1\"",
+	"noNativeZipCommand"           => "na serveri nieje možné vykonať príkaz \"zip\"",
+	"permissionDeniedUpload"       => "prístup odoprený pre zápis do tohoto adresára",
+	"permissionDeniedCreateDirectory"=>"prístupodoprený: nieje možné vytvoriť adresár",
+	"permissionDeniedCreateFile"   => "prístup odoprený: nieje možné vytvoriť súbor",
+	"permissionDeniedDeleteDirectory"=>"prístup odoprený: nieje možné zmazať adresár",
+	"permissionDeniedDeleteFile"   => "prístup odoprený: nieje možné zmazať súbor",
+	"permissionDeniedEditDirectory"=> "prístup odoprený: nieje možné editovať adresár",
+	"permissionDeniedEditFile"     => "prístup odoprený: nieje možné editovať súbor",
+	"permissionDeniedMoveDirectory"=> "prístup odoprený: nieje možné presunúť adresár",
+	"permissionDeniedMoveFile"     => "prístup odoprený: nieje možné presunúť súbor",
+	"permissionDeniedRename"       => "prístup odoprený: nieje možné premenovať \"%1\"",
+	"permissionDeniedViewFile"     => "prístup odoprený: nieje možné zobraziť súbor",
+	"searchResults"                => "výsledky vyhľadávania",
+	# 1.2
+	"fileNotCreatedDirUnwritable"    => "\"%1\" nieje možné vytvoriť, adresár nieje otvorený pre zápis",
+	"fileNotDeletedUnwritable"       => "\"%1\" nieje možné zmazať.",
+	"fileNotMovableUnwritable"       => "\"%1\" nieje možné presunúť",
+	"permissionDeniedManipImage"     => "prístup odoprený: nieje možné manipulovať s obrázkom",
+	"imageNotWritable"               => "obrázok nieje možné zapísať",
+	"failedGetFileObject"            => "nepodarilo sa vrátiť File object",
+	"failedGetDirectoryObject"       => "nepodarilo sa vrátiť Directory object",
+	"failedDeleteFile"               => "zlyhalo zmazanie \"%1\"",
+	"failedMoveFile"                 => "zlyhal presun \"%1\"",
+	"failedWriteToFile"              => "zlyhal zápis do \"%1\"",
+	"failedDownloadFromUrl"        => "zlyhalo stiahnutie súboru z \"%1\""
+	,
+	"file saved"                     => "súbor uložený",
+	"files deleted"                  => "%1 súbory boli zmazané",
+	"errorZipContainsBannedFilename" => "chyba: archív obsahuje súbory s nepovolenými koncovkami",
+	"error: filename not allowed"    => "chyba: názov súboru nieje povolený",
+	"error: url must begin with http"=> "chyba: url musí začínať s http://",
+	"UNKNOWN TAG"                    => "NEZNÁMY TAG \"%1\"",
+	"error: unzip failed"            => "chyba: zlyhal príkaz unzip"
+);
+?>
