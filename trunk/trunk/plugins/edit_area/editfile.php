aacfd03d4ad3e742d2457d45a0ab6645a1411068
bterkuile <bterkuile@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2009-09-14 17:27:45 +0000

    add missing peaces of blt theme, I dont understand why not there in the first place Added multi file file editing with edit area

--- trunk/trunk/plugins/edit_area/editfile.php
+++ trunk/trunk/plugins/edit_area/editfile.php
@@ -1,45 +1,58 @@
 <?php
 require_once('../../initialise.php');
+$file_ids = $kfm_session->get('editarea_files');
 if(!isset($_GET['fid']))die ('no file id specified');
+if(!$file_ids){
+  $file_ids = explode(",", $_GET['fid']);
+}else{
+  $file_ids = explode(',', $file_ids);
+  $file_ids = array_merge($file_ids, explode(",", $_GET['fid']));
+}
+$file_ids = array_unique($file_ids);
+$kfm_session->set('editarea_files', implode(',', $file_ids));
+
 $f=new kfmFile($_GET['fid']);
 if(!$f)die ('error retrieving file');
-switch($f->getExtension()){
-	case 'html':
-		$lang='html';
-		break;
-	case 'c':
-		$lang='c';
-		break;
-	case 'cpp':
-		$lang='cpp';
-		break;
-	case 'css':
-		$lang='css';
-		break;
-	case 'js':
-		$lang='js';
-		break;
-	case 'pas':
-		$lang='pas';
-		break;
-	case 'php':
-		$lang='php';
-		break;
-	case 'python':
-		$lang='python';
-		break;
-	case 'sql':
-		$lang='sql';
-		break;
-	case 'vb':
-		$lang='vb';
-		break;
-	case 'xml':
-		$lang='xml';
-		break;
-	default:
-		$lang='';
-		break;
+function getSyntax($ext){
+	switch($ext){
+		case 'html':
+			$lang='html';
+			break;
+		case 'c':
+			$lang='c';
+			break;
+		case 'cpp':
+			$lang='cpp';
+			break;
+		case 'css':
+			$lang='css';
+			break;
+		case 'js':
+			$lang='js';
+			break;
+		case 'pas':
+			$lang='pas';
+			break;
+		case 'php':
+			$lang='php';
+			break;
+		case 'python':
+			$lang='python';
+			break;
+		case 'sql':
+			$lang='sql';
+			break;
+		case 'vb':
+			$lang='vb';
+			break;
+		case 'xml':
+			$lang='xml';
+			break;
+		default:
+			$lang='';
+			break;
+	}
+  return $lang;
 }
 ?>
 <DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -47,6 +60,7 @@ switch($f->getExtension()){
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>EditArea - the code editor in a textarea</title>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js"></script>
 	<script language="Javascript" type="text/javascript" src="edit_area/edit_area_full.js"></script>
 	<script language="Javascript" type="text/javascript">
 		editAreaLoader.init({
@@ -55,17 +69,32 @@ switch($f->getExtension()){
 			,allow_resize: "both"
 			,allow_toggle: true
 			,language: "en"
-			,syntax: "<?php echo $lang;?>"
 			,toolbar: "save,charmap,|,search,go_to_line,|,undo,redo,|,select_font,|,syntax_selection,|, change_smooth_selection,highlight,reset_highlight,|,help"
 			,save_callback:"save_document"
 			,plugins: "charmap"
 			,charmap_default: "arrows"
+      ,is_multi_files: true
+      ,EA_load_callback: "load_ea_files"
+      ,EA_file_close_callback: "close_ea_file"
 		});
 		function save_document(id, content){
-			parent.x_kfm_saveTextFile(<?php echo $f->id;?>,content, function(res){
+      var f = editAreaLoader.getCurrentFile("editareacontent");
+			parent.x_kfm_saveTextFile(f.id, content, function(res){
+        editAreaLoader.setFileEditedMode("editareacontent", String(f.id), false);
 				//parent.kfm_pluginIframeMessage('document saved');	
 			});
 		}
+    function load_ea_files(element_id){
+      <?php foreach($file_ids as $fid){
+        $f = kfmFile::getInstance($fid);
+        print "
+        editAreaLoader.openFile('editareacontent', {id:$fid,text:'".str_replace("'", "\\'", $f->getContent())."',syntax:'".getSyntax($f->getExtension())."',title:'".$f->name."'});";
+      }?>
+    }
+    function close_ea_file(f){
+      jQuery.get("remove_file.php?fid="+f.id, function(res){eval(res);});
+      return true;
+    }
 	</script>
 <style type="text/css">
 *{
@@ -75,6 +104,6 @@ switch($f->getExtension()){
 </style>
 <head>
 <body>
-<textarea id="editareacontent" style="width:100%;height:90%;"><?php echo $f->getContent();?></textarea>
+<textarea id="editareacontent" style="width:100%;height:90%;"></textarea>
 </body>
 </html>
--- trunk/trunk/plugins/edit_area/plugin.js
+++ trunk/trunk/plugins/edit_area/plugin.js
@@ -2,12 +2,12 @@ function plugin_edit_area(){
 	this.name='edit_area';
 	this.title='Edit with edit area';
 	this.category='edit';
-	this.mode=0; //only one file
+	this.mode=2; //only one file
 	this.writable=1; // only writable files
 	this.extensions=['html','c','cpp','css','js', 'pas','php', 'python', 'sql', 'vb', 'xml','txt'];
 	this.doFunction=function(files){
-		fid=files[0];
-		kfm_pluginIframeShow('plugins/edit_area/editfile.php?fid='+fid);
+		//fid=files[0];
+		kfm_pluginIframeShow('plugins/edit_area/editfile.php?fid='+files.join(','));
 	}
 }
 kfm_addHook(new plugin_edit_area());
