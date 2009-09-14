146c603ff8e0cfa9a80b9d9d446d4cc41c75df85
bterkuile <bterkuile@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2009-09-14 11:13:29 +0000

    add doctype to admin panel for IE compatibility

--- trunk/admin/index.php
+++ trunk/admin/index.php
@@ -10,6 +10,7 @@ $getparams='?';
 foreach($_GET as $key => $value)$getparams.=urlencode($key).'='.urlencode($value).'&';
 $getparams=rtrim($getparams,'& ');
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
 <title>KFM admin</title>
