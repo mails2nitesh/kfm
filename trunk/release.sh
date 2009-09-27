3686372bacb200a86b21c36c03f6b7e99e1c9fff
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2009-09-27 08:49:37 +0000

    add Bash version of release.sh, for use on Linux machines

---
+++ trunk/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# This script performs some operations to make KFM release ready
+
+# Remove .svn directories
+find . -name .svn -exec rm {} \;
+
+# Remove some plugins
+for plugin in codepress return_thumbnail
+do
+	rm -rf plugins/$plugin
+done
+
+# Remove some jquery files
+rm -f j/jquery/jquery.impromptu.js
+
+# And last but not least, remove the release scripts
+rm release.sh
