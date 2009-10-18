8fff1ec24d0a94c6c423ee2160f89193bdab2398
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2009-10-18 15:12:45 +0000

    plugins should always return values in JSON. including errors!

--- kfm2/plugins/get_file_listing/get_file_listing.php
+++ kfm2/plugins/get_file_listing/get_file_listing.php
@@ -3,9 +3,9 @@ function get_file_listing(){
 	$req_dir=isset($_REQUEST['d'])?$_REQUEST['d']:'';
 	$d=KFM_USERDIR .'/'. $req_dir;
 
-	if(preg_match('/\.\./',$d))die('no hacking please');
-	if(!file_exists($d))die('directory <b>'.$d.'</b> does not exist');
-	if(!is_dir($d))die('requested directory is not a directory');
+	if(preg_match('/\.\./',$d))die('{"error":1,"error_message":"no hacking please"}');
+	if(!file_exists($d))die('{"error":2,"error_message":directory <b>'.$d.'</b> does not exist"}');
+	if(!is_dir($d))die('{"error":3,"error_message":"requested directory is not a directory"}');
 
 	$files=array();
 	$directories=array();
