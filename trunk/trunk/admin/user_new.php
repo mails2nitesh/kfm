6655677b9eb7960b1c763c2f2c48e9f6031edf9f
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2009-11-19 09:34:06 +0000

    fix more of those count() bugs (http://code.google.com/p/kfm/issues/detail?id=28) also, add some SQL to clean out expired session data

--- trunk/trunk/admin/user_new.php
+++ trunk/trunk/admin/user_new.php
@@ -4,7 +4,7 @@ require_once('functions.php');
 if($kfm->user_status!=1)die ('error("No authorization aquired")');
 if(!(isset($_POST['username'])&&isset($_POST['password'])))die('$.prompt("error with new user request");');
 $res=db_fetch_all('SELECT id FROM '.KFM_DB_PREFIX.'users WHERE username="'.$_POST['username'].'"');
-if(count($res))die('$.prompt("Name already exists.");');
+if($res && count($res))die('$.prompt("Name already exists.");');
 $kfm->db->query('INSERT INTO '.KFM_DB_PREFIX.'users (username, password,status) VALUES ("'.$_POST['username'].'", "'.sha1($_POST['password']).'", 2)');
 if(PEAR::isError($kfmdb))die('$.prompt("error with database interactions");');
 $uhtml=user_row($kfm->db->lastInsertId(),$_POST['username'],2);
