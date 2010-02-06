f11d69719f4b95e4e4632fe42dcdc3fad0fa556d
kae.verens <kae.verens@2ebc7e00-e71e-0410-ae5f-cd0be9aec2aa>
2010-02-06 14:41:55 +0000

    remove some unnecessary files from the jquery directory

--- trunk/release.rb
+++ trunk/release.rb
@@ -9,8 +9,5 @@ rm_rf Dir.["**/.svn/"]
 # Remove some plugins
 chdir("plugins"){ rm_rf(%w{codepress return_thumbnail})
 
-# Remove some jquery files
-chdir("j/jquery"){ rm_rf(%w{jquery.impromptu.js}) }
-
 # And last but not least, remove this script
 rm_rf $0
